d977147b2b58072e183a3e94dc571ed5afac51e3
kalvin <kalvin.vasconcellos@gmail.com>
2020-11-29 22:16:45 -0300

    server: add graceful shutdown

--- server/server.go
+++ server/server.go
@@ -1,10 +1,63 @@
 package server
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func setupGracefulShutdown(server *http.Server) {
+	signalListener := make(chan os.Signal)
+
+	signal.Notify(signalListener, syscall.SIGINT, syscall.SIGTERM)
+	<-signalListener
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+		os.Exit(1)
+	}
+
+	log.Println("Server exiting...")
+
+	os.Exit(0)
+}
+
+func startServer(server *http.Server) {
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Server failed to initialize")
+
+		os.Exit(1)
+	}
+}
+
 // Run app
 func Run() {
-	var port = ":3000"
+	var address = ":3000"
 
 	router := initializeRouter()
 
-	router.Run(port)
+	server := &http.Server{
+		Addr:    address,
+		Handler: router,
+	}
+
+	go startServer(server)
+
+	log.Println("Server is up and kicking")
+
+	setupGracefulShutdown(server)
 }
