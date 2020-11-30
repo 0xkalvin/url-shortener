39740d2873f538c2ae9c63df4022d27f2742647a
kalvin <kalvin.vasconcellos@gmail.com>
2020-11-29 22:49:42 -0300

    project: initialize databases

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/0xkalvin/url-shortener/database"
 	"github.com/0xkalvin/url-shortener/server"
 )
 
 func main() {
+
+	database.InitializeDynamoDB()
+	database.InitializeRedis()
 
 	server.Run()
 }
